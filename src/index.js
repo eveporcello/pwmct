e5c81e74f7adc3f2a83bda9fde3ce6c1f80cb6ef
eveporcello <eve@moonhighway.com>
2020-09-19 10:34:13 -0700

    Adds TV

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const { ApolloServer, gql } = require("apollo-server");
 const { RESTDataSource } = require("apollo-datasource-rest");
 const fetch = require("node-fetch");
 
+require("dotenv").config();
+
 const typeDefs = gql`
   type Person {
     id: ID!
@@ -16,14 +18,21 @@ const typeDefs = gql`
     id: ID!
   }
   type TV {
-    id: ID!
+    sku: String!
+    name: String!
+    salePrice: Float!
+  }
+  enum TvSort {
+    SKU
+    NAME
+    SALEPRICE
   }
   type Query {
     person: Person!
     woman: Woman!
     man: Man!
     camera: Camera!
-    tv: TV!
+    tv(sortBy: TvSort!): TV!
   }
 `;
 
@@ -41,7 +50,15 @@ class RonSwansonAPI extends RESTDataSource {
 const resolvers = {
   Query: {
     man: async (parent, args, { dataSources }) =>
-      dataSources.ronAPI.getQuote()
+      dataSources.ronAPI.getQuote(),
+    tv: async (parent, { sortBy }) => {
+      const television = await fetch(
+        `https://api.bestbuy.com/v1/products(categoryPath.name="All%20Flat-Screen%20TVs")?format=json&show=sku,name,salePrice&sort=${sortBy}&apiKey=${process.env.BB_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data.products[0]);
+      return television;
+    }
   }
 };
 
