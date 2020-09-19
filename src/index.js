51c33da59a0fe70a50c509e5764b1db078127ad8
eveporcello <eve@moonhighway.com>
2020-09-19 12:48:35 -0700

    adds working person queries

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { RESTDataSource } = require("apollo-datasource-rest");
 const fetch = require("node-fetch");
+const MongoClient = require("mongodb").MongoClient;
 
 require("dotenv").config();
 
 const typeDefs = gql`
   type Person {
-    id: ID!
+    _id: ID!
+    username: String!
+    name: String!
+    address: String!
+    birthdate: String!
+    email: String!
   }
   type Woman {
     id: ID!
@@ -15,61 +21,91 @@ const typeDefs = gql`
     quote: String!
   }
   type Camera {
-    id: ID!
+    sku: String!
+    name: String!
+    salePrice: Float!
   }
   type TV {
     sku: String!
     name: String!
     salePrice: Float!
   }
-  enum TvSort {
+  enum SortCategories {
     SKU
     NAME
-    SALEPRICE
   }
   type Query {
-    person: Person!
+    person(username: String!): Person!
     woman: Woman!
     man: Man!
-    camera: Camera!
-    tv(sortBy: TvSort!): TV!
+    camera(sortBy: SortCategories): Camera!
+    tv(sortBy: SortCategories!): TV!
   }
 `;
 
-class RonSwansonAPI extends RESTDataSource {
+class BestBuyAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = "https://ron-swanson-quotes.herokuapp.com/v2";
+    this.baseURL = "https://api.bestbuy.com/v1";
   }
-  async getQuote() {
-    let data = await this.get("/quotes");
-    return { quote: data[0] };
+  async getTV(sortBy) {
+    const tv = await this.get(
+      `/products(categoryPath.name=TVs)?format=json&show=sku,name,salePrice,description&sort=${sortBy}&apiKey=${process.env.BB_API_KEY}`
+    ).then((data) => data.products[0]);
+    return tv;
+  }
+  async getCamera(sortBy) {
+    const camera = await this.get(
+      `/products(categoryPath.name=digital%20cameras)?format=json&show=sku,name,salePrice,description&sort=${sortBy}&apiKey=${process.env.BB_API_KEY}`
+    ).then((data) => data.products[0]);
+    return camera;
   }
 }
 
 const resolvers = {
   Query: {
-    man: async (parent, args, { dataSources }) =>
-      dataSources.ronAPI.getQuote(),
-    tv: async (parent, { sortBy }) => {
-      const television = await fetch(
-        `https://api.bestbuy.com/v1/products(categoryPath.name="All%20Flat-Screen%20TVs")?format=json&show=sku,name,salePrice&sort=${sortBy}&apiKey=${process.env.BB_API_KEY}`
-      )
-        .then((res) => res.json())
-        .then((data) => data.products[0]);
-      return television;
-    }
+    person: async (parent, { username }, { people }) => {
+      return people.findOne({ username });
+    },
+    man: async () => {
+      const quoteArray = await fetch(
+        "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+      ).then((res) => res.json());
+      return {
+        quote: quoteArray[0]
+      };
+    },
+    tv: async (parent, { sortBy }, { dataSources }) =>
+      dataSources.bestBuyAPI.getTV(sortBy),
+    camera: async (parent, { sortBy }, { dataSources }) =>
+      dataSources.bestBuyAPI.getCamera(sortBy)
   }
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => ({
-    ronAPI: new RonSwansonAPI()
-  })
-});
+const start = async () => {
+  const uri = process.env.MONGO_URI;
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  const db = client.db();
+  const context = async () => {
+    const people = db.collection("customers");
+    return { people };
+  };
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+    dataSources: () => ({
+      bestBuyAPI: new BestBuyAPI()
+    })
+  });
+  server
+    .listen()
+    .then(({ url }) =>
+      console.log(`Apollo Server running at ${url}`)
+    );
+};
 
-server
-  .listen()
-  .then(({ url }) => console.log(`Apollo Server running at ${url}`));
+start();
